bc27516970ce74220879b93c9cd0096c1076d679
kingBook <412126604@qq.com>
2022-01-17 23:30:30 +0800

    create

---
+++ Assets/BezierUtil.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BezierUtil {
+
+    public static void Bezier1 (Vector3 p0, Vector3 p1, float t, out Vector3 output) {
+        output = p0 + (p1 - p0) * t;
+    }
+
+    public static Vector3[] GetBezierPoints1 (Vector3 p0, Vector3 p1, int segmentNum, bool inclusiveStartEnd) {
+        List<Vector3> points = new List<Vector3>();
+        for (int i = 1; i <= segmentNum; i++) {
+            float t = i / (float)segmentNum;
+            Bezier1(p0, p1, t, out Vector3 point);
+            points.Add(point);
+        }
+        if (inclusiveStartEnd) {
+            points.Insert(0, p0);
+            points.Add(p1);
+        }
+        return points.ToArray();
+    }
+
+    public static void Bezier2 (Vector3 p0, Vector3 p1, Vector3 p2, float t, out Vector3 output) {
+        output = (1f - t) * (1f - t) * p0 + 2f * t * (1f - t) * p1 + t * t * p2;
+    }
+
+    public static Vector3[] GetBezierPoints2 (Vector3 p0, Vector3 p1, Vector3 p2, int segmentNum, bool inclusiveStartEnd) {
+        List<Vector3> points = new List<Vector3>();
+        for (int i = 1; i <= segmentNum; i++) {
+            float t = i / (float)segmentNum;
+            Bezier2(p0, p1, p2, t, out Vector3 point);
+            points.Add(point);
+        }
+        if (inclusiveStartEnd) {
+            points.Insert(0, p0);
+            points.Add(p2);
+        }
+        return points.ToArray();
+    }
+
+    public static void Bezier3 (Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t, out Vector3 output) {
+        Vector3 a = (1f - t) * (1f - t) * (1f - t) * p0;
+        Vector3 b = 3f * t * (1f - t) * (1f - t) * p1;
+        Vector3 c = 3f * t * t * (1f - t) * p2;
+        Vector3 d = t * t * t * p3;
+        output = a + b + c + d;
+    }
+
+    public static Vector3[] GetBezierPoints3 (Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, int segmentNum, bool inclusiveStartEnd) {
+        List<Vector3> points = new List<Vector3>();
+        for (int i = 1; i <= segmentNum; i++) {
+            float t = i / (float)segmentNum;
+            Bezier3(p0, p1, p2, p3, t, out Vector3 point);
+            points.Add(point);
+        }
+        if (inclusiveStartEnd) {
+            points.Insert(0, p0);
+            points.Add(p3);
+        }
+        return points.ToArray();
+    }
+}
---
+++ Assets/TestBezier.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBezier : MonoBehaviour {
+    public Transform[] transforms;
+    void Start () {
+
+    }
+
+    void Update () {
+        Vector3[] points1 = BezierUtil.GetBezierPoints1(transforms[0].position, transforms[1].position, 5, true);
+        DrawPoints(points1, Color.red);
+
+        Vector3[] points2 = BezierUtil.GetBezierPoints2(transforms[0].position, transforms[1].position, transforms[2].position, 5, true);
+        DrawPoints(points2, Color.green);
+
+
+        Vector3[] points3 = BezierUtil.GetBezierPoints3(transforms[0].position, transforms[1].position, transforms[2].position, transforms[3].position, 5, true);
+        DrawPoints(points3, Color.blue);
+    }
+
+    private void DrawPoints (Vector3[] points, Color color) {
+        for (int i = 0, len = points.Length; i < len; i++) {
+            if (i < len - 2) {
+                Debug.DrawLine(points[i], points[i + 1], color);
+            }
+        }
+    }
+
+}
